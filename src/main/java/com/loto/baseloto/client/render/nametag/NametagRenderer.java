10d1ab717ef560e029f70135fd0f10353230f623
Drew <drew.ciaramitaro51@gmail.com>
2018-11-12 21:16:02 -0500

    Added AA to OL Minion Tags & refactored how they render

--- src/main/java/com/loto/baseloto/client/render/nametag/NametagRenderer.java
+++ src/main/java/com/loto/baseloto/client/render/nametag/NametagRenderer.java
@@ -1,5 +1,7 @@
 package com.loto.baseloto.client.render.nametag;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
@@ -47,12 +49,14 @@ public class NametagRenderer {
 
 			float entityHeight = entityIn.height + 0.5F;
 			drawNameplate(name, (float) x, (float) y + entityHeight, (float) z,
-					(float) yaw, (float) pitch, Math.sqrt(dist), renderThroughBlocks);
+					(float) yaw, (float) pitch, Math.sqrt(dist),
+					renderThroughBlocks);
 		}
 	}
 
 	public void drawNameplate(String name, float x, float y, float z,
-			float viewerYaw, float viewerPitch, double distance, boolean renderThroughBlocks) {
+			float viewerYaw, float viewerPitch, double distance,
+			boolean renderThroughBlocks) {
 		double distanceFromPlayer = Math.min(32, distance);
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(x, y, z);
@@ -63,8 +67,8 @@ public class NametagRenderer {
 				* distanceFromPlayer * .45, 0.025F * distanceFromPlayer * .55);
 		GlStateManager.disableLighting();
 		GlStateManager.depthMask(false);
-		
-		if(renderThroughBlocks) {
+
+		if (renderThroughBlocks) {
 			GlStateManager.disableDepth();
 		}
 
@@ -76,26 +80,26 @@ public class NametagRenderer {
 						GlStateManager.DestFactor.ZERO);
 		int i = this.renderManager.getFontRenderer().getStringWidth(name) / 2;
 		GlStateManager.disableTexture2D();
-		Tessellator tessellator = Tessellator.getInstance();
-		BufferBuilder bufferbuilder = tessellator.getBuffer();
-		bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
-		bufferbuilder.pos((double) (-i - 1), -1, 0.0D)
-				.color(0.1F, 0.1F, 0.1F, 0.3F).endVertex();
-		bufferbuilder.pos((double) (-i - 1), 8, 0.0D)
-				.color(0.1F, 0.1F, 0.1F, 0.3F).endVertex();
-		bufferbuilder.pos((double) (i + 1), 8, 0.0D)
-				.color(0.1F, 0.1F, 0.1F, 0.3F).endVertex();
-		bufferbuilder.pos((double) (i + 1), -1, 0.0D)
-				.color(0.1F, 0.1F, 0.1F, 0.3F).endVertex();
-		tessellator.draw();
+
+		GL11.glEnable(GL11.GL_POLYGON_SMOOTH);
+		GL11.glHint(GL11.GL_POLYGON_SMOOTH_HINT, GL11.GL_NICEST);
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glColor4f(0.1F, 0.1F, 0.1F, 0.3F);
+		GL11.glVertex2d((-i - 1), -1);
+		GL11.glVertex2d((-i - 1), 8);
+		GL11.glVertex2d((i + 1), 8);
+		GL11.glVertex2d((i + 1), -1);
+		GL11.glColor4f(1F, 1F,1F, 1F);
+		GL11.glEnd();
+		GL11.glDisable(GL11.GL_POLYGON_SMOOTH);
 
 		GlStateManager.enableTexture2D();
 
 		this.renderManager.getFontRenderer().drawString(name,
 				-this.renderManager.getFontRenderer().getStringWidth(name) / 2,
 				0, -1);
-		
-		if(renderThroughBlocks) {
+
+		if (renderThroughBlocks) {
 			GlStateManager.enableDepth();
 		}
 
@@ -105,4 +109,5 @@ public class NametagRenderer {
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		GlStateManager.popMatrix();
 	}
+
 }
